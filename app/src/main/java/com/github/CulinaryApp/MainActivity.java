5c194eff2afd788c65307bfcd729f944d71a09c0
m-lambo <mal6@email.sc.edu>
2021-11-16 17:48:44 -0500

    Modified sign-in button to the google sign-in button, still working on integrating firebase auth.

--- app/src/main/java/com/github/CulinaryApp/MainActivity.java
+++ app/src/main/java/com/github/CulinaryApp/MainActivity.java
@@ -2,84 +2,82 @@ package com.github.CulinaryApp;
 
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView numInputField = findViewById(R.id.numInput);
+        TextView emailInputField = findViewById(R.id.emailInput);
         TextView passInputField = findViewById(R.id.passInput);
 
-
-
-
-        Button login = findViewById(R.id.loginButton);
-        // Configure sign-in to request the user's ID, email address, and basic
-// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // Configure sign-in to request user's ID & email address
         GoogleSignInOptions gso = new GoogleSignInOptions
                 .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 // this is the backend server's  OAuth 2.0 client ID
                 .requestIdToken("453772724489-hcig30pfqiddt4af1i8ucu2766kuiikh.apps.googleusercontent.com")
                 .requestEmail()
                 .build();
-        /*       login.setOnClickListener(view -> {
 
-           boolean numRight = numInputField.getText().toString().equals(correctPhone);
-           boolean passRight = passInputField.getText().toString().equals(correctPass);
+        // init GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-            boolean numRight = true;
-            boolean passRight = true;
-
-            if (numRight && passRight)
-                output("Login Success", "Welcome, Chef Cocky!");
-            else
-                output("Login Failed", "Check Phone Number or Password. (Phone: 8438675309, Pass: capstone)");
-
-        });
-
- */
-       
-        /*
-            if (userExists(numInputField,passInputField))
-                navigateToCategoriesPage();
-         */
-    }
-    /*
-    private boolean userExists(TextView number, TextView password) {
-        
-        // if (user account is authenticated by firebase instance
-        return true;
-       
+        // init Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
 
-    public void navigateToCategoriesPage() {
-        Intent intentToStartCategoriesPage = new Intent(this, CategoriesActivity.class);
-        startActivity(intentToStartCategoriesPage);
+    public void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        // check for existing google sign in account, if the user is already signed in,
+        // the GoogleSignInAccount will be non-null
+        // transition them to categories screen if non-null
+
     }
-    private void output(String title, String msg){
-        AlertDialog.Builder alertBox = new AlertDialog.Builder(this);
-        alertBox.setTitle(title);
-        alertBox.setMessage(msg);
 
-        alertBox.setPositiveButton("Ok", null);
-
-        alertBox.show();
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+     //   startActivityForResult(signInIntent, RC_SIGN_IN);
     }
-    */
-
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            // ...
+        }
+    }
 }
+
