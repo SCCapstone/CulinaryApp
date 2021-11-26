8a123f2df347146aa9c42bc78811b15b41938928
m-lambo <mal6@email.sc.edu>
2021-11-25 22:37:54 -0500

    Removed google sign in button, going w/ FirebaseUI auth solution

--- app/src/main/java/com/github/CulinaryApp/views/LoginActivity.java
+++ app/src/main/java/com/github/CulinaryApp/views/LoginActivity.java
@@ -3,15 +3,22 @@ package com.github.CulinaryApp.views;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.github.CulinaryApp.R;
-
-
 public class LoginActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(com.github.CulinaryApp.R.layout.activity_login);
+
     }
+
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    private void signIn() {
+    }
+
 }
