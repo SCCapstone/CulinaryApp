0fdd0d1045f4ed4df55a743505a8a63628738a5a
m-lambo <mal6@email.sc.edu>
2021-11-25 19:35:24 -0500

    Modified some resource names in res/ & reflected that in MainActivity calls to the res/ file

--- app/src/main/java/com/github/CulinaryApp/MainActivity.java
+++ app/src/main/java/com/github/CulinaryApp/MainActivity.java
@@ -2,75 +2,67 @@ package com.github.CulinaryApp;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AlertDialog;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView numInputField = findViewById(R.id.numInput);
-        TextView passInputField = findViewById(R.id.passInput);
+        TextView usersEmail = findViewById(R.id.usersEmail);
+        TextView usersPassword = findViewById(R.id.usersPassword);
 
+        // Configure sign-in to request user's ID & email address
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                // this is the backend server's  OAuth 2.0 client ID
+                .requestIdToken("453772724489-hcig30pfqiddt4af1i8ucu2766kuiikh.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
 
+        // init GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-
-        Button login = findViewById(R.id.loginButton);
-        /*       login.setOnClickListener(view -> {
-
-           boolean numRight = numInputField.getText().toString().equals(correctPhone);
-           boolean passRight = passInputField.getText().toString().equals(correctPass);
-
-            boolean numRight = true;
-            boolean passRight = true;
-
-            if (numRight && passRight)
-                output("Login Success", "Welcome, Chef Cocky!");
-            else
-                output("Login Failed", "Check Phone Number or Password. (Phone: 8438675309, Pass: capstone)");
-
-        });
-
- */
-       
-        /*
-            if (userExists(numInputField,passInputField))
-                navigateToCategoriesPage();
-         */
-    }
-    /*
-    private boolean userExists(TextView number, TextView password) {
-        
-        // if (user account is authenticated by firebase instance
-        return true;
-       
+        // init Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
 
-    public void navigateToCategoriesPage() {
-        Intent intentToStartCategoriesPage = new Intent(this, CategoriesActivity.class);
-        startActivity(intentToStartCategoriesPage);
+    public void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        // check for existing google sign in account, if the user is already signed in,
+        // the GoogleSignInAccount will be non-null
+        // transition them to categories screen if non-null
+
     }
-    private void output(String title, String msg){
-        AlertDialog.Builder alertBox = new AlertDialog.Builder(this);
-        alertBox.setTitle(title);
-        alertBox.setMessage(msg);
 
-        alertBox.setPositiveButton("Ok", null);
-
-        alertBox.show();
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        //   startActivityForResult(signInIntent, RC_SIGN_IN);
     }
-    */
-
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            // ...
+        }
+    }
 }
