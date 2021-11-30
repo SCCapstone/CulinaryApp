f292099f68627cb0942aa92c8f2259816f62b23f
Ralph J. Gleaton <ralphg@email.sc.edu>
2021-11-30 15:17:18 -0500

    Small changes to Registration, trying to get firebase working

--- app/src/main/java/com/github/CulinaryApp/RegistrationActivity.java
+++ app/src/main/java/com/github/CulinaryApp/RegistrationActivity.java
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class RegistrationActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
-    Button nButton;
-    EditText emailAddress;
-    EditText password;
-    EditText confPassword;
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button nButton;
+    private EditText emailAddress;
+    private EditText password;
+    private EditText confPassword;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -22,36 +26,51 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        mAuth = FirebaseAuth.getInstance();
+
         nButton = (Button)findViewById(R.id.button);
         emailAddress = (EditText)findViewById(R.id.EmailAddress);
         password = (EditText)findViewById(R.id.Password);
         confPassword = (EditText)findViewById(R.id.ConfPassword);
+    }
 
-        nButton.setOnClickListener(
-            new View.OnClickListener()
-            {
-                public void onClick(View view)
-                {
-                    //Check to see if any field is blank, display error message if so
-                    if(emailAddress.getText().toString().matches("") ||
-                    password.getText().toString().matches("") ||
-                    confPassword.getText().toString().matches(""))
-                    {
-                        Toast.makeText(RegistrationActivity.this, "One or more fields is empty", Toast.LENGTH_SHORT).show();
-                    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
 
-                    //Check to make sure both password fields match
-                    else if(!password.getText().toString().matches(confPassword.getText().toString())){
-                        Toast.makeText(RegistrationActivity.this, "Please make sure both passwords match", Toast.LENGTH_SHORT).show();
-                    }
-
-                    //TODO Check to make sure email is unique
-                    else{
-                        startActivity(new Intent(RegistrationActivity.this, RegPage2Activity.class));
-                        setContentView(R.layout.activity_reg_page2);
-                    }
-
+                //If email address doesn't match standard email format throw error
+                if(Patterns.EMAIL_ADDRESS.matcher(emailAddress.getText().toString().trim()).matches()){
+                    emailAddress.setError("Please enter a valid email address!");
+                    emailAddress.requestFocus();
+                    return;
                 }
-            });
+
+                //Firebase doesn't accept passwords less than 6 characters
+                if(password.getText().toString().trim().length() < 6){
+                    password.setError("Password must be at least 6 characters long!");
+                    password.requestFocus();
+                    return;
+                }
+
+                if(emailAddress.getText().toString().isEmpty()||
+                        password.getText().toString().isEmpty() ||
+                        confPassword.getText().toString().isEmpty())
+                {
+                    Toast.makeText(RegistrationActivity.this, "One or more fields is empty", Toast.LENGTH_SHORT).show();
+                }
+
+                //Check to make sure both password fields match
+                else if(!password.getText().toString().matches(confPassword.getText().toString())){
+                    Toast.makeText(RegistrationActivity.this, "Please make sure both passwords match", Toast.LENGTH_SHORT).show();
+                }
+
+                //TODO Check to make sure email is unique
+                else{
+                    startActivity(new Intent(RegistrationActivity.this, RegPage2Activity.class));
+                    //setContentView(R.layout.activity_reg_page2);
+                }
+                break;
+        }
     }
 }
