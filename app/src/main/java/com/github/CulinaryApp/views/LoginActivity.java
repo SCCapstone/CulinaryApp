8a2cc14a71bf6df6a15e2e9922333735478c6772
m-lambo <mal6@email.sc.edu>
2021-11-30 18:14:50 -0500

    Accidently called navigateToCategoriesPage in that last commit

--- app/src/main/java/com/github/CulinaryApp/views/LoginActivity.java
+++ app/src/main/java/com/github/CulinaryApp/views/LoginActivity.java
@@ -103,7 +103,7 @@ public class LoginActivity extends AppCompatActivity {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                navigateToCategoriesPage(v);
+                navigateToRegistrationPage(v);
             }
         });
 
