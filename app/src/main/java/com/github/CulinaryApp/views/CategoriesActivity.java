34c5a7efa6be6e831d9b3e82bef9779a96466e1c
Michael Sana <msana@email.sc.edu>
2021-12-01 19:00:00 -0500

    Create CategoriesActivity.java

---
+++ app/src/main/java/com/github/CulinaryApp/views/CategoriesActivity.java
@@ -0,0 +1,68 @@
+package com.github.CulinaryApp.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.github.CulinaryApp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+public class CategoriesActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+
+        //Code for toolbar
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        //displays home button
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    //This method is what should send to recipes page
+    public void sendMessage(View view) {
+        //Intent intent = new Intent(this, Recipe.java.class);
+        //startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_action_bar, menu);
+        MenuItem searchItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setQueryHint("Search");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                return false;
+            }
+        });
+        return true;
+    }
+
+}
