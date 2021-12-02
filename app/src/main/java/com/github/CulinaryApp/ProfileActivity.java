67e267d7b83b4fc52f1086e70b2723215723262d
Lukacs Ablonczy <lukacsa@email.sc.edu>
2021-12-02 15:14:46 +0100

    profile popup and disappearace done

--- app/src/main/java/com/github/CulinaryApp/ProfileActivity.java
+++ app/src/main/java/com/github/CulinaryApp/ProfileActivity.java
@@ -2,8 +2,10 @@ package com.github.CulinaryApp;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -12,6 +14,8 @@ import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import com.github.dhaval2404.imagepicker.ImagePicker;
@@ -21,6 +25,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class ProfileActivity extends AppCompatActivity {
     private CircleImageView prof;
     private ImageView bgImg;
+    private FragmentContainerView clipboardContainer;
     boolean changingProfPic, changingBgImg;
 
 
@@ -31,12 +36,12 @@ public class ProfileActivity extends AppCompatActivity {
         changingBgImg = false;
         changingProfPic = false;
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .setReorderingAllowed(true)
-                    .add(R.id.clipboardFragmentHolder, ClipboardFragment.class, null)
-                    .commit();
-        }
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .setReorderingAllowed(true)
+//                    .add(R.id.clipboardFragmentHolder, ClipboardFragment.class, null)
+//                    .commit();
+//        }
 
     }
 
@@ -53,18 +58,46 @@ public class ProfileActivity extends AppCompatActivity {
         Button bgChangeButton = findViewById(R.id.editBGImg);
         bgChangeButton.setOnClickListener(bgImgChanger);
 
-        FragmentContainerView clipboardContainer = findViewById(R.id.clipboardFragmentHolder);
-        clipboardContainer.setOnFocusChangeListener((container, hasFocus) -> {
-            if(!hasFocus)
-                container.setVisibility(View.INVISIBLE);
-        });
+        clipboardContainer = findViewById(R.id.clipboardFragmentHolder);
+        clipboardContainer.setOnFocusChangeListener(clipboardFocusListener);
 
         Button clipboardButton = findViewById(R.id.toolbarClip);
-        clipboardButton.setOnClickListener(v -> {
+        clipboardButton.setOnClickListener(toggleClipboardVis);
+
+
+        LinearLayout content = findViewById(R.id.contentLayout);
+
+
+        Button searchButton = findViewById(R.id.toolbarSearch);
+        searchButton.setOnClickListener(v -> System.out.println(this.getCurrentFocus()));
+
+    }
+
+    View.OnFocusChangeListener clipboardFocusListener = (container, hasFocus) -> {
+        if(!hasFocus)
+            container.setVisibility(View.GONE);
+    };
+
+    View.OnClickListener toggleClipboardVis = dummy -> {
+        FragmentManager manager = getSupportFragmentManager();
+        int visibility = clipboardContainer.getVisibility();
+
+        if(visibility==View.GONE){
+            manager.beginTransaction()
+                    .add(R.id.clipboardFragmentHolder, ClipboardFragment.class, null)
+                    .commit();
+
             clipboardContainer.setVisibility(View.VISIBLE);
             clipboardContainer.requestFocus();
-        });
-    }
+        } else if (visibility == View.VISIBLE){
+            manager.beginTransaction()
+                    .remove(manager.findFragmentById(R.id.clipboardFragmentHolder))
+                    .commit();
+
+            clipboardContainer.setVisibility(View.GONE);
+
+        }
+    };
 
     View.OnClickListener profImgChanger = view -> {
         changingProfPic = true;
@@ -87,6 +120,8 @@ public class ProfileActivity extends AppCompatActivity {
                 .maxResultSize(1080, 1080)
                 .start();
     };
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
