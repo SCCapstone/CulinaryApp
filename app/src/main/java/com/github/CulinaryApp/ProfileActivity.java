834d4d9b3e23d7b0ec18f7ec5e9dea49f2f6b4ec
Lukacs Ablonczy <lukacsa@email.sc.edu>
2021-12-03 14:31:40 +0100

    nearly done with page

--- app/src/main/java/com/github/CulinaryApp/ProfileActivity.java
+++ app/src/main/java/com/github/CulinaryApp/ProfileActivity.java
@@ -1,18 +1,21 @@
 package com.github.CulinaryApp;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentContainerView;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -27,6 +30,7 @@ public class ProfileActivity extends AppCompatActivity {
     private ImageView bgImg;
     private FragmentContainerView clipboardContainer;
     boolean changingProfPic, changingBgImg;
+    private String bio, displayName;
 
 
     @Override
@@ -35,11 +39,13 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         changingBgImg = false;
         changingProfPic = false;
+        bio = "No Bio yet...";
+        displayName = "New User...";
 
 //        if (savedInstanceState == null) {
 //            getSupportFragmentManager().beginTransaction()
 //                    .setReorderingAllowed(true)
-//                    .add(R.id.clipboardFragmentHolder, ClipboardFragment.class, null)
+//                    .add(R.id.profileDisplayContainer, ProfileDisplayFragment.class, null)
 //                    .commit();
 //        }
 
@@ -52,26 +58,92 @@ public class ProfileActivity extends AppCompatActivity {
         bgImg = findViewById(R.id.profBackground);
         prof = findViewById(R.id.profPic);
 
-        Button profChangeButton = findViewById(R.id.editAvatar);
-        profChangeButton.setOnClickListener(profImgChanger);
-
-        Button bgChangeButton = findViewById(R.id.editBGImg);
-        bgChangeButton.setOnClickListener(bgImgChanger);
-
         clipboardContainer = findViewById(R.id.clipboardFragmentHolder);
-        clipboardContainer.setOnFocusChangeListener(clipboardFocusListener);
-
-        Button clipboardButton = findViewById(R.id.toolbarClip);
-        clipboardButton.setOnClickListener(toggleClipboardVis);
-
-
         LinearLayout content = findViewById(R.id.contentLayout);
 
-
+        Button profChangeButton = findViewById(R.id.editAvatar);
+        Button bgChangeButton = findViewById(R.id.editBGImg);
+        Button clipboardButton = findViewById(R.id.toolbarClip);
+        Button editBioButton = findViewById(R.id.editBio);
+        Button editDisplayNameButton = findViewById(R.id.editDispName);
+        Button profileDisplayButton = findViewById(R.id.howTheySee);
         Button searchButton = findViewById(R.id.toolbarSearch);
+
+        profChangeButton.setOnClickListener(profImgChanger);
+        bgChangeButton.setOnClickListener(bgImgChanger);
+        clipboardButton.setOnClickListener(toggleClipboardVis);
+        editBioButton.setOnClickListener(bioEditor);
+        editDisplayNameButton.setOnClickListener(displayNameEditor);
+        profileDisplayButton.setOnClickListener(displayProfileFull);
+
+        clipboardContainer.setOnFocusChangeListener(clipboardFocusListener);
+
+        //todo delete or replace
         searchButton.setOnClickListener(v -> System.out.println(this.getCurrentFocus()));
 
     }
+
+    public String[] getProfileStrings(){
+        return new String[] {displayName, bio};
+    }
+
+    public ConstraintLayout getImages(){
+        return findViewById(R.id.images);
+    }
+
+    View.OnClickListener displayProfileFull = view ->{
+//        findViewById(R.id.clipboardFragmentHolder).setVisibility(View.VISIBLE);
+
+        FragmentManager manager = getSupportFragmentManager();
+
+        manager.beginTransaction()
+                .add(R.id.profileDisplayContainer, ProfileDisplayFragment.class, null)
+//                .addSharedElement(bgImg, "bgImg")
+//                .addSharedElement(prof, "prof")
+                .commit();
+    };
+
+    private String textFromDialog(String title, String hint){
+        final String[] textRetrieved = {"Default Text"};
+
+        AlertDialog.Builder textInputDialog = new AlertDialog.Builder(this);
+        textInputDialog.setTitle(title);
+
+        EditText newTextInput = new EditText(ProfileActivity.this);
+        newTextInput.setHint(hint);
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        int dpPaddingSides = 32;
+        float scale = getResources().getDisplayMetrics().density;
+        int pixelPaddingSides = (int) (dpPaddingSides*scale + 0.5f);
+        layout.setPadding(pixelPaddingSides, pixelPaddingSides, pixelPaddingSides, pixelPaddingSides);
+
+        layout.addView(newTextInput);
+
+        textInputDialog.setView(layout);
+
+        textInputDialog.setPositiveButton("Done", (dialog, choice) -> {
+            textRetrieved[0] = newTextInput.getText().toString();
+        });
+
+        textInputDialog.setNegativeButton("Cancel", (dialog, choice) -> {
+            dialog.cancel();
+        });
+
+        textInputDialog.show();
+
+        return textRetrieved[0];
+    }
+
+    View.OnClickListener displayNameEditor = view -> {
+        displayName = textFromDialog("Edit Display Name", "New Display Name Here");
+    };
+
+    View.OnClickListener bioEditor = view -> {
+        bio = textFromDialog("Edit Bio", "Your Bio Here");
+    };
 
     View.OnFocusChangeListener clipboardFocusListener = (container, hasFocus) -> {
         if(!hasFocus)
