5d157276019d2bedfc7c4a6af751d2a3e1f2e78f
Lukacs Ablonczy <lukacsa@email.sc.edu>
2021-12-03 15:57:25 +0100

    toolbar done minus search

--- app/src/main/java/com/github/CulinaryApp/ProfileActivity.java
+++ app/src/main/java/com/github/CulinaryApp/ProfileActivity.java
@@ -3,23 +3,18 @@ package com.github.CulinaryApp;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentContainerView;
 import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.HasDefaultViewModelProviderFactory;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.Switch;
 import android.widget.Toast;
 
 import com.github.dhaval2404.imagepicker.ImagePicker;
@@ -60,22 +55,25 @@ public class ProfileActivity extends AppCompatActivity {
 
         Button profChangeButton = findViewById(R.id.editAvatar);
         Button bgChangeButton = findViewById(R.id.editBGImg);
-        Button clipboardButton = findViewById(R.id.toolbarClip);
         Button editBioButton = findViewById(R.id.editBio);
         Button editDisplayNameButton = findViewById(R.id.editDispName);
         Button profileDisplayButton = findViewById(R.id.howTheySee);
 
+        Button clipboardButton = findViewById(R.id.toolbarClip);
         Button trendingButton = findViewById(R.id.toolbarTrending);
-        Button searchButton = findViewById(R.id.toolbarSearch);
+//        Button searchButton = findViewById(R.id.toolbarSearch);
 
         Button healthSettingsButton = findViewById(R.id.setHealth);
         Button lifestyleSettingsButton = findViewById(R.id.setLifestyle);
         Button activitySettingsButton = findViewById(R.id.setActivity);
         Button privacySettingsButton = findViewById(R.id.setPrivacy);
 
+        clipboardButton.setOnClickListener(toggleClipboard);
+        trendingButton.setOnClickListener(toggleTrending);
+//        searchButton.setOnClickListener(v -> System.out.println(this.getCurrentFocus()));
+
         profChangeButton.setOnClickListener(profImgChanger);
         bgChangeButton.setOnClickListener(bgImgChanger);
-        clipboardButton.setOnClickListener(toggleClipboardVis);
         editBioButton.setOnClickListener(bioEditor);
         editDisplayNameButton.setOnClickListener(displayNameEditor);
         profileDisplayButton.setOnClickListener(displayProfileFull);
@@ -85,12 +83,58 @@ public class ProfileActivity extends AppCompatActivity {
         activitySettingsButton.setOnClickListener(activitySettingsChanger);
         privacySettingsButton.setOnClickListener(privacySettingsChanger);
 
-        clipboardContainer.setOnFocusChangeListener(clipboardFocusListener);
+        clipboardContainer.setOnFocusChangeListener(toolbarFocusListener);
+        trendingContainer.setOnFocusChangeListener(toolbarFocusListener);
 
-        //todo delete or replace
-        searchButton.setOnClickListener(v -> System.out.println(this.getCurrentFocus()));
 
     }
+
+    View.OnFocusChangeListener toolbarFocusListener = (container, hasFocus) -> {
+        if(!hasFocus)
+            container.setVisibility(View.GONE);
+    };
+
+    View.OnClickListener toggleTrending = view -> {
+        FragmentManager manager = getSupportFragmentManager();
+        int visibility = trendingContainer.getVisibility();
+
+        if(visibility==View.GONE){
+            manager.beginTransaction()
+                    .add(R.id.trendingFragmentHolder, TrendingFragment.class, null)
+                    .commit();
+
+            trendingContainer.setVisibility(View.VISIBLE);
+            trendingContainer.requestFocus();
+        } else if (visibility == View.VISIBLE){
+            manager.beginTransaction()
+                    .remove(manager.findFragmentById(R.id.trendingFragmentHolder))
+                    .commit();
+
+            trendingContainer.setVisibility(View.GONE);
+
+        }
+    };
+
+    View.OnClickListener toggleClipboard = dummy -> {
+        FragmentManager manager = getSupportFragmentManager();
+        int visibility = clipboardContainer.getVisibility();
+
+        if(visibility==View.GONE){
+            manager.beginTransaction()
+                    .add(R.id.clipboardFragmentHolder, ClipboardFragment.class, null)
+                    .commit();
+
+            clipboardContainer.setVisibility(View.VISIBLE);
+            clipboardContainer.requestFocus();
+        } else if (visibility == View.VISIBLE){
+            manager.beginTransaction()
+                    .remove(manager.findFragmentById(R.id.clipboardFragmentHolder))
+                    .commit();
+
+            clipboardContainer.setVisibility(View.GONE);
+
+        }
+    };
 
     View.OnClickListener healthSettingsChanger = view -> {
         androidx.appcompat.widget.SwitchCompat glutenSwitch = new androidx.appcompat.widget.SwitchCompat(this);
@@ -141,32 +185,6 @@ public class ProfileActivity extends AppCompatActivity {
 
     View.OnClickListener bioEditor = view -> {
         bio = textFromDialog("Edit Bio", "Your Bio Here");
-    };
-
-    View.OnFocusChangeListener clipboardFocusListener = (container, hasFocus) -> {
-        if(!hasFocus)
-            container.setVisibility(View.GONE);
-    };
-
-    View.OnClickListener toggleClipboardVis = dummy -> {
-        FragmentManager manager = getSupportFragmentManager();
-        int visibility = clipboardContainer.getVisibility();
-
-        if(visibility==View.GONE){
-            manager.beginTransaction()
-                    .add(R.id.clipboardFragmentHolder, ClipboardFragment.class, null)
-                    .commit();
-
-            clipboardContainer.setVisibility(View.VISIBLE);
-            clipboardContainer.requestFocus();
-        } else if (visibility == View.VISIBLE){
-            manager.beginTransaction()
-                    .remove(manager.findFragmentById(R.id.clipboardFragmentHolder))
-                    .commit();
-
-            clipboardContainer.setVisibility(View.GONE);
-
-        }
     };
 
     View.OnClickListener profImgChanger = view -> {
