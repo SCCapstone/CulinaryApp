6d970212aa4a941a1829fc399c5867d1c0923254
m-lambo <mal6@email.sc.edu>
2022-01-29 23:09:32 -0500

    Added behavior test for existing user attempting to login

---
+++ app/src/test/java/com/github/CulinaryApp/BehaviorTests.java
@@ -0,0 +1,65 @@
+/*
+package com.github.CulinaryApp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.util.Log;
+
+import com.github.CulinaryApp.views.*;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExternalResource;
+import androidx.test.ext.junit.rules.*;
+import androidx.test.ext.junit.rules.ActivityScenerioRule; // to simulate activity launch
+import static android.support.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.ext.truth.content.IntentSubject.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import com.google.common.collect.Iterables; // to simulate intent
+
+@RunsWith(AndroidJUnit4.class)
+public class BehaviorTests {
+    String userInput1;
+    String userInput2;
+    String userInput3;
+    String userInput4; // ..., ...
+
+    /**
+     * Simulate UI navigation using ActivityScenerioRule
+     */
+
+    /* First test simulates Login interactions, given an existing user
+
+    @Rule
+    ActivityScenerioRule rule = new ActivityScenerioRule<>(LoginActivity.class);
+
+    private void enterInfo(String anEmail, String aPassword) {
+
+        onView(R.id.email).perform(typeText(anEmail), closeSoftKeyboard());
+        onView(R.id.password).perform(typeText(aPassword), closeSoftKeyboard());
+    }
+
+    public boolean nextActivitySuccesfullyLaunched(Class<CategoriesActivity> theNextActivity) {
+        return assertTrue(Iterables.getOnlyElement(Intents.getIntents())).hasComponentClass(theNextActivity);
+    }
+
+    @Test
+    public void authenticatedUserLogsIn() {
+
+        userInput1 = "email@gmail.com";
+        userInput2 = "email@gmail";
+        enterInfo(userInput1, userInput2);
+
+        assertTrue(nextActivitySuccesfullyLaunched(CategoriesActivity.class));
+
+    }
+
+
+}
+*/
