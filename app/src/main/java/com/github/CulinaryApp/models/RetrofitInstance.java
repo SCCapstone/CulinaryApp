5de8f37c912aa84a0e64fb00c81bc3123d44fd86
m-lambo <mal6@email.sc.edu>
2022-01-30 18:41:33 -0500

    Removed androidx dependencies from now, commented out retrofit class and executed a test

--- app/src/main/java/com/github/CulinaryApp/models/RetrofitInstance.java
+++ app/src/main/java/com/github/CulinaryApp/models/RetrofitInstance.java
@@ -1,11 +1,19 @@
 package com.github.CulinaryApp.models;
 
+import okhttp3.HttpUrl;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
-    Retrofit retrofit = Retrofit.Builder()
-            .baseUrl("https://www.themealdb.com/api.php/json/v1/1/categories.php")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
+/*
+    public static final String BASE_URL = "https://www.themealdb.com/api/";
+    public Retrofit getRetrofit() {
+        Retrofit retrofit = Retrofit.Builder
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit;
+    }
+
+ */
 }
