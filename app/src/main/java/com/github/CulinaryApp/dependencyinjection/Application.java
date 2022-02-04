21bb2a2026afd14a17716bbaa284f016c342165d
m-lambo <mal6@email.sc.edu>
2022-02-03 21:22:03 -0500

    Refactored, disambiguated services & repository file locations, removed RetrofitInstance & moved Retrofit Builder to Application.java

--- app/src/main/java/com/github/CulinaryApp/dependencyinjection/Application.java
+++ app/src/main/java/com/github/CulinaryApp/dependencyinjection/Application.java
@@ -2,7 +2,12 @@ package com.github.CulinaryApp.dependencyinjection;
 
 import dagger.hilt.android.HiltAndroidApp;
 
+import okhttp3.HttpUrl;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 @HiltAndroidApp
 public class Application {
+
 
 }
