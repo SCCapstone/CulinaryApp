fa2bc234077ad675730ef606d382bbe041db24fc
lablonczy <lukeablonczy@hotmail.com>
2022-02-25 17:48:04 -0500

    changed favorites to activity

--- app/src/main/java/com/github/CulinaryApp/NavbarFragment.java
+++ app/src/main/java/com/github/CulinaryApp/NavbarFragment.java
@@ -38,43 +38,17 @@ public class NavbarFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
-        Button clipboardFavs = getView().findViewById(R.id.toolbarFavs);
+        Button favsButton = getView().findViewById(R.id.toolbarFavs);
         Button homeButtom = getView().findViewById(R.id.toolbarHome);
         Button profileButton = getView().findViewById(R.id.toolbarProfile);
 //        Button trendingButton = getView().findViewById(R.id.toolbarTrending);
 
-        clipboardFavs.setOnClickListener(toggleFavs);
         profileButton.setOnClickListener(new navListener(ProfileActivity.class));
+        favsButton.setOnClickListener(new navListener(FavoritesActivity.class));
         homeButtom.setOnClickListener(new navListener(CategoriesActivity.class));
 //        trendingButton.setOnClickListener(toggleTrending);
     }
 
-    View.OnClickListener toggleFavs = view -> {
-        Activity currentActivity = getActivity();
-
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        FragmentContainerView favsContainer = (FragmentContainerView) inflater.inflate(R.layout.layout_navbar_favs_cont, currentActivity.findViewById(R.id.favsFragmentHolder));
-        currentActivity.addContentView(favsContainer, new LinearLayoutCompat.LayoutParams(LinearLayoutCompat.LayoutParams.WRAP_CONTENT, LinearLayoutCompat.LayoutParams.WRAP_CONTENT));
-
-        FragmentManager manager = getActivity().getSupportFragmentManager();
-        int visibility = favsContainer.getVisibility();
-
-        if(visibility==View.GONE){
-            manager.beginTransaction()
-                    .add(R.id.favsFragmentHolder, TrendingFragment.class, null)
-                    .commit();
-
-            favsContainer.setVisibility(View.VISIBLE);
-            favsContainer.requestFocus();
-        } else if (visibility == View.VISIBLE){
-            manager.beginTransaction()
-                    .remove(manager.findFragmentById(R.id.favsFragmentHolder))
-                    .commit();
-
-            favsContainer.setVisibility(View.GONE);
-
-        }
-    };
 
     private class navListener <T extends Activity> implements View.OnClickListener {
         private final Class<T> targetActivity;
@@ -95,24 +69,5 @@ public class NavbarFragment extends Fragment {
         }
     }
 
-//        =
-//    } view -> {
-//        if(getActivity() instanceof CategoriesActivity)
-//            return;
-//
-//        Intent goToProfile = new Intent(getContext(), CategoriesActivity.class);
-//        startActivity(goToProfile);
-//
-//        getActivity().finish();
-//    };
 
-//    private View.OnClickListener navToProf = view -> {
-//        if(getActivity() instanceof ProfileActivity)
-//            return;
-//
-//        Intent goToProfile = new Intent(getContext(), ProfileActivity.class);
-//        startActivity(goToProfile);
-//
-//        getActivity().finish();
-//    };
 }
